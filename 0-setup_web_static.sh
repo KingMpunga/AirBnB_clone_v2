dcf5de91c6093e06041888d18edea1ee15366f2b
Jean <mpunga.djodjo@gmail.com>
2024-04-07 13:49:33 -0700

    web static change

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,27 +1,42 @@
-#!/usr/bin/env bash
-# Bash script that sets up web servers for the deployment of web_static
-sudo apt-get update
-sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
+#!/bin/bash
 
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "<html>
+# Install nginx if not already installed
+apt-get update
+apt-get -y install nginx
+
+# Create necessary directories if they don't exist
+mkdir -p /data/web_static/{releases/test,shared}
+
+# Create a fake HTML file for testing
+echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+</html>" > /data/web_static/releases/test/index.html
 
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+# Create symbolic link /data/web_static/current
+rm -rf /data/web_static/current
+ln -s /data/web_static/releases/test/ /data/web_static/current
 
-sudo chown -R ubuntu:ubuntu /data/
+# Give ownership of /data/ folder to ubuntu user and group recursively
+chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+# Configure Nginx to serve content of /data/web_static/current to hbnb_static
+server_block="/etc/nginx/sites-available/holberton"
+echo "server {
+    listen 80;
+    server_name _;
 
-sudo service nginx restart
+    location /hbnb_static {
+        alias /data/web_static/current/;
+    }
+}" > "$server_block"
+ln -sf "$server_block" /etc/nginx/sites-enabled/
+
+# Restart Nginx to apply changes
+service nginx restart
+
+exit 0
+
